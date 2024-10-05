cc36ea024be96092b36fa57796bb0351f4046edf
Uchio Kondo <udzura@udzura.jp>
2024-10-05 12:41:26 +0900

    微調整

--- samples/hello-wasm/src/lib.rs
+++ samples/hello-wasm/src/lib.rs
@@ -1,4 +1,9 @@
+extern "C" {
+    fn hoge(hoge: i32);
+}
+
 #[no_mangle]
-pub fn add(left: i32, right: i32) -> i32 {
+pub unsafe fn add(left: i32, right: i32) -> i32 {
+    hoge(left);
     left + right
 }
